ef3a5e8c3a2b28a5bf35375af55ab942605fbc6b
haifeng.jiang <autel@autels-MacBook-Pro.local>
2026-03-14 16:33:29 +0800

    add log

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
 from lark import Lark
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 load_dotenv()
 
 def main():
     larkClient = Lark(os.getenv("APP_ID"), os.getenv("APP_SECRET"))
     employees = larkClient.list_employee()
+    logger.info(f"Found {len(employees)} employees from ehr")
     bitableEmployees = larkClient.list_bitable_employee(os.getenv("BITABLE_ID"), os.getenv("TABLE_ID"))
+    logger.info(f"Found {len(bitableEmployees)} bitable employees from bitable")
     newEmployees = []
     for employee in employees:
         if employee not in bitableEmployees.keys():
